e6875d6a0edf952bf013b4a3440f079718ad56a5
Azwel9nj <mulumbesimwinga@gmail.com>
2022-09-04 01:34:14 +0200

    Updated ui

--- originalfacerec.py
+++ originalfacerec.py
@@ -13,6 +13,19 @@ import pandas as pd
 import datetime
 import time
 import detect
+from pathlib import Path
+
+# from tkinter import *
+# Explicit imports to satisfy Flake8
+from tkinter import Tk, Canvas, Entry, Text, Button, PhotoImage
+
+
+OUTPUT_PATH = Path(__file__).parent
+ASSETS_PATH = OUTPUT_PATH / Path("./assets")
+
+
+def relative_to_assets(path: str) -> Path:
+    return ASSETS_PATH / Path(path)
 #import time
 # Copyright 2021 The TensorFlow Authors. All Rights Reserved.
 #
@@ -216,7 +229,7 @@ def clear():
     txt.delete(0, 'end')
     txt2.delete(0, 'end')
     res = "1)Take Images  ===> 2)Save Profile"
-    message1.configure(text=res)
+    #message1.configure(text=res)
 
 #Check for correct Path
 def assure_path_exists(path):
@@ -437,11 +450,11 @@ def TakeImages():
             writer = csv.writer(csvFile)
             writer.writerow(row)
         csvFile.close()
-        message1.configure(text=res)
+        #message1.configure(text=res)
     else:
         if (name.isalpha() == False):
             res = "Enter Correct name"
-            message.configure(text=res)
+            #message.configure(text=res)
 ########################################################################################
 #$$$$$$$$$$$$$
 def TrainImages():
@@ -462,8 +475,8 @@ def TrainImages():
         return
     recognizer.save("Pass_Train/Trainner.yml")
     res = "Profile Saved Successfully"
-    message1.configure(text=res)
-    message.configure(text='Total Registrations till now  : ' + str(ID[0]))
+    #message1.configure(text=res)
+    #message.configure(text='Total Registrations till now  : ' + str(ID[0]))
 
 ############################################################################################3
 #$$$$$$$$$$$$$
@@ -505,8 +518,8 @@ def TrackImages():
     assure_path_exists("StudentDetails/")
     assure_path_exists("AttendanceImages/")
     currentDateTime = datetime.datetime.now()
-    for k in tb.get_children():
-        tb.delete(k)
+    """for k in tb.get_children():
+        tb.delete(k)"""
     msg = ''
     i = 0
     j = 0
@@ -612,131 +625,215 @@ def TrackImages():
         if (cv2.waitKey(1) == ord('q')):
             break
                     
-    csvFile1.close()
+    #csvFile1.close()
     cam.release()
     cv2.destroyAllWindows()
 
 #Front End===========================================================
 
-window = tkinter.Tk()
+window = Tk()
 window.title("Face and Facemask Recognition Based Attendance System")
-window.geometry("1280x720")
-window.resizable(True,True)
-window.configure(background='#355454')
-
-#Help menubar----------------------------------------------
-menubar=Menu(window)
-help=Menu(menubar,tearoff=0)
-help.add_command(label="Change Password!",command=change_pass)
-help.add_command(label="Contact Us",command=contact)
-help.add_separator()
-help.add_command(label="Exit",command=on_closing)
-menubar.add_cascade(label="Help",menu=help)
-
-# add ABOUT label to menubar-------------------------------
-menubar.add_command(label="About",command=about)
-
-#This line will attach our menu to window
-window.config(menu=menubar)
-
-#main window------------------------------------------------
-message3 = tkinter.Label(window, text="Face and Facemask Recognition Based Attendance System" ,fg="white",bg="#355454" ,width=60 ,height=1,font=('times', 29, ' bold '))
-message3.place(x=10, y=10,relwidth=1)
-
-#frames-------------------------------------------------
-frame1 = tkinter.Frame(window, bg="white")
-frame1.place(relx=0.11, rely=0.15, relwidth=0.39, relheight=0.80)
-
-frame2 = tkinter.Frame(window, bg="white")
-frame2.place(relx=0.51, rely=0.15, relwidth=0.39, relheight=0.80)
-
-#frame_headder
-fr_head1 = tkinter.Label(frame1, text="Register New Student", fg="white",bg="black" ,font=('times', 17, ' bold ') )
-fr_head1.place(x=0,y=0,relwidth=1)
-
-fr_head2 = tkinter.Label(frame2, text="Student's attendance", fg="white",bg="black" ,font=('times', 17, ' bold ') )
-fr_head2.place(x=0,y=0,relwidth=1)
-
-#registretion frame
-lbl = tkinter.Label(frame1, text="Enter ID",width=20  ,height=1  ,fg="black"  ,bg="white" ,font=('times', 17, ' bold ') )
-lbl.place(x=0, y=55)
-
-txt = tkinter.Entry(frame1,width=32 ,fg="black",bg="#e1f2f2",highlightcolor="#00aeff",highlightthickness=3,font=('times', 15, ' bold '))
-txt.place(x=55, y=88,relwidth=0.75)
-
-lbl2 = tkinter.Label(frame1, text="Enter Name",width=20  ,fg="black"  ,bg="white" ,font=('times', 17, ' bold '))
-lbl2.place(x=0, y=140)
-
-txt2 = tkinter.Entry(frame1,width=32 ,fg="black",bg="#e1f2f2",highlightcolor="#00aeff",highlightthickness=3,font=('times', 15, ' bold ')  )
-txt2.place(x=55, y=173,relwidth=0.75)
-
-message0=tkinter.Label(frame1,text="Follow the steps...",bg="white" ,fg="black"  ,width=39 ,height=1,font=('times', 16, ' bold '))
-message0.place(x=7,y=275)
-
-message1 = tkinter.Label(frame1, text="1)Take Images  ===> 2)Save Profile" ,bg="white" ,fg="black"  ,width=39 ,height=1, activebackground = "yellow" ,font=('times', 15, ' bold '))
-message1.place(x=7, y=300)
-
-message = tkinter.Label(frame1, text="" ,bg="white" ,fg="black"  ,width=39,height=1, activebackground = "yellow" ,font=('times', 16, ' bold '))
-message.place(x=7, y=500)
-#Attendance frame
-lbl3 = tkinter.Label(frame2, text="Attendance Table",width=20  ,fg="black"  ,bg="white"  ,height=1 ,font=('times', 17, ' bold '))
-lbl3.place(x=100, y=115)
-
-#Display total registration----------
-res=0
-exists = os.path.isfile("StudentDetails/StudentDetails.csv")
-if exists:
-    with open("StudentDetails/StudentDetails.csv", 'r') as csvFile1:
-        reader1 = csv.reader(csvFile1)
-        for l in reader1:
-            res = res + 1
-    res = (res // 2) - 1
-    csvFile1.close()
-else:
-    res = 0
-message.configure(text='Total Registrations : '+str(res))
-
-#BUTTONS----------------------------------------------
-
-clearButton = tkinter.Button(frame1, text="Clear", command=clear, fg="white", bg="#13059c", width=11, activebackground = "white", font=('times', 12, ' bold '))
-clearButton.place(x=55, y=230,relwidth=0.29)
-
-takeImg = tkinter.Button(frame1, text="Take Images", command=TakeImages, fg="black", bg="#00aeff", width=34, height=1, activebackground = "white", font=('times', 16, ' bold '))
-takeImg.place(x=30, y=350,relwidth=0.89)
-
-trainImg = tkinter.Button(frame1, text="Save Profile", command=psw, fg="black", bg="#00aeff", width=34, height=1, activebackground = "white", font=('times', 16, ' bold '))
-trainImg.place(x=30, y=430,relwidth=0.89)
-
-trackImg = tkinter.Button(frame2, text="Take Attendance", command=TrackImages, fg="black", bg="#00aeff", height=1, activebackground = "white" ,font=('times', 16, ' bold '))
-trackImg.place(x=30,y=60,relwidth=0.89)
-
-quitWindow = tkinter.Button(frame2, text="Quit", command=window.destroy, fg="white", bg="#13059c", width=35, height=1, activebackground = "white", font=('times', 16, ' bold '))
-quitWindow.place(x=30, y=450,relwidth=0.89)
+window.geometry("1223x729")
+window.configure(bg = "#FFFFFF")
 
 
+canvas = Canvas(
+    window,
+    bg = "#FFFFFF",
+    height = 729,
+    width = 1223,
+    bd = 0,
+    highlightthickness = 0,
+    relief = "ridge"
+)
 
-#Attandance table----------------------------
-style = ttk.Style()
-style.configure("mystyle.Treeview", highlightthickness=0, bd=0, font=('Calibri', 11)) # Modify the font of the body
-style.configure("mystyle.Treeview.Heading",font=('times', 13,'bold')) # Modify the font of the headings
-style.layout("mystyle.Treeview", [('mystyle.Treeview.treearea', {'sticky': 'nswe'})]) # Remove the borders
-tb= ttk.Treeview(frame2,height =13,columns = ('name','date','time'),style="mystyle.Treeview")
-tb.column('#0',width=82)
-tb.column('name',width=130)
-tb.column('date',width=133)
-tb.column('time',width=133)
-tb.grid(row=2,column=0,padx=(0,0),pady=(150,0),columnspan=4)
-tb.heading('#0',text ='ID')
-tb.heading('name',text ='NAME')
-tb.heading('date',text ='DATE')
-tb.heading('time',text ='TIME')
+canvas.place(x = 0, y = 0)
+canvas.create_rectangle(
+    0.0,
+    0.0,
+    422.0,
+    729.0,
+    fill="#FC5353",
+    outline="")
 
-#SCROLLBAR--------------------------------------------------
+image_image_1 = PhotoImage(
+    file=relative_to_assets("image_1.png"))
+image_1 = canvas.create_image(
+    211.0,
+    259.0,
+    image=image_image_1
+)
 
-scroll=ttk.Scrollbar(frame2,orient='vertical',command=tb.yview)
-scroll.grid(row=2,column=4,padx=(0,100),pady=(150,0),sticky='ns')
-tb.configure(yscrollcommand=scroll.set)
+entry_image_1 = PhotoImage(
+    file=relative_to_assets("entry_1.png"))
+entry_bg_1 = canvas.create_image(
+    823.0,
+    319.5,
+    image=entry_image_1
+)
+txt = Entry(
+    bd=0,
+    bg="#D9D9D9",
+    highlightthickness=0
+)
+txt.place(
+    x=529.0,
+    y=300.0,
+    width=588.0,
+    height=37.0
+)
 
-#closing lines------------------------------------------------
-window.protocol("WM_DELETE_WINDOW", on_closing)
+entry_image_2 = PhotoImage(
+    file=relative_to_assets("entry_2.png"))
+entry_bg_2 = canvas.create_image(
+    823.0,
+    160.5,
+    image=entry_image_2
+)
+txt2 = Entry(
+    bd=0,
+    bg="#D9D9D9",
+    highlightthickness=0
+)
+txt2.place(
+    x=529.0,
+    y=141.0,
+    width=588.0,
+    height=37.0
+)
+
+button_image_1 = PhotoImage(
+    file=relative_to_assets("button_1.png"))
+quitWindow  = Button(
+    image=button_image_1,
+    borderwidth=0,
+    highlightthickness=0,
+    command=window.destroy,
+    relief="flat"
+)
+quitWindow .place(
+    x=57.0,
+    y=591.0,
+    width=307.34075927734375,
+    height=45.0
+)
+
+button_image_2 = PhotoImage(
+    file=relative_to_assets("button_2.png"))
+trackImg = Button(
+    image=button_image_2,
+    borderwidth=0,
+    highlightthickness=0,
+    command=TrackImages,
+    relief="flat"
+)
+trackImg.place(
+    x=57.0,
+    y=439.0,
+    width=307.0,
+    height=45.0
+)
+
+button_image_3 = PhotoImage(
+    file=relative_to_assets("button_3.png"))
+takeImg  = Button(
+    image=button_image_3,
+    borderwidth=0,
+    highlightthickness=0,
+    command=TakeImages,
+    relief="flat"
+)
+takeImg.place(
+    x=517.0,
+    y=589.0,
+    width=240.0,
+    height=45.0
+)
+
+button_image_4 = PhotoImage(
+    file=relative_to_assets("button_4.png"))
+trainImg = Button(
+    image=button_image_4,
+    borderwidth=0,
+    highlightthickness=0,
+    command=psw,
+    relief="flat"
+)
+trainImg.place(
+    x=889.0,
+    y=591.0,
+    width=240.0,
+    height=45.0
+)
+
+canvas.create_text(
+    753.0,
+    259.0,
+    anchor="nw",
+    text="USER NAME",
+    fill="#000000",
+    font=("Inter", 24 * -1)
+)
+
+canvas.create_text(
+    743.0,
+    100.0,
+    anchor="nw",
+    text="NRC NUMBER",
+    fill="#000000",
+    font=("Inter", 24 * -1)
+)
+
+canvas.create_text(
+    14.0,
+    20.0,
+    anchor="nw",
+    text="FACE MASK AND FACIAL",
+    fill="#FFFFFF",
+    font=("Inter", 32 * -1)
+)
+
+canvas.create_text(
+    87.0,
+    59.0,
+    anchor="nw",
+    text="RECOGNITION",
+    fill="#FFFFFF",
+    font=("Inter", 32 * -1)
+)
+
+canvas.create_text(
+    91.0,
+    98.0,
+    anchor="nw",
+    text="ATTENDANCE",
+    fill="#FFFFFF",
+    font=("Inter", 32 * -1)
+)
+
+canvas.create_text(
+    699.0,
+    20.0,
+    anchor="nw",
+    text="REGISTER USER",
+    fill="#000000",
+    font=("Inter SemiBold", 32 * -1)
+)
+
+button_image_5 = PhotoImage(
+    file=relative_to_assets("button_5.png"))
+clearButton = Button(
+    image=button_image_5,
+    borderwidth=0,
+    highlightthickness=0,
+    command=clear,
+    relief="flat"
+)
+clearButton.place(
+    x=521.0,
+    y=438.0,
+    width=240.0,
+    height=46.3365478515625
+)
+window.resizable(False, False)
 window.mainloop()
