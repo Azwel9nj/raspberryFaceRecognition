f545e4a408b041972dcf7ea3611e8522b717e09c
Azwel9nj <mulumbesimwinga@gmail.com>
2022-09-04 17:35:17 +0200

    Gotten TaKE Images to respond

--- originalfacerec.py
+++ originalfacerec.py
@@ -365,22 +365,13 @@ def getProfile(id):
     return profile
 #$$$$$$$$$$$$$
 def TakeImages():
+    
     name = (txt2.get())
     check_haarcascadefile()
     columns = ['SERIAL NO.', '', 'ID', '', 'NAME']
     assure_path_exists("StudentDetails/")
-    assure_path_exists("TrainingImage/")
-    #Make name sub directory
-    parent_dir = "TrainingImage/"
-    #Custom Path Per User
-    #path = os.path.join(parent_dir, name)
-    #os.mkdir(path)
-    ###Database Option Code
-    """storeImage = im = open(img_name, 'rb').read()
-    conn.execute("INSERT INTO images(name, img, createdOn) VALUES(?,?,?)",(img_name , sqlite3.Binary(storeImage),currentDateTime))
-    print("{} written!".format(img_name))
-    conn.commit()"""
-    ###Database Option Code End
+    assure_path_exists("TrainingImage/")    
+    parent_dir = "TrainingImage/"    
     serial = 0
     exists = os.path.isfile("StudentDetails/StudentDetails.csv")
     if exists:
@@ -397,70 +388,64 @@ def TakeImages():
             serial = 1
         csvFile1.close()
     Id = (txt.get())
+    
+    
     serial = Id
     currentDateTime = datetime.datetime.now()
     conn.execute("INSERT INTO users(name, userId, createdOn) VALUES(?,?,?)",(name ,Id ,currentDateTime))
-    if ((name.isalpha()) or (' ' in name)):
-        cam = cv2.VideoCapture(0)
-        harcascadePath = "haarcascade_frontalface_default.xml"
-        detector = cv2.CascadeClassifier(harcascadePath)
-        sampleNum = 0
-        t_end = 1
-        while True:
-            ret, img = cam.read()
-            gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-            faces = detector.detectMultiScale(gray, 1.3, 5)
-            for (x, y, w, h) in faces:
-                #currentDateTime = datetime.datetime.now()
-                cv2.rectangle(img, (x, y), (x + w, y + h), (0, 255, 0), 2)
-                # incrementing sample number
-                sampleNum = sampleNum + 1
-                # saving the captured face in the dataset folder TrainingImage
-                cv2.imwrite("TrainingImage/ " + name + "." + str(serial) + "." + Id + '.' + str(sampleNum) + ".jpg",
-                            gray[y:y + h, x:x + w])
-                # display the frame
-                cv2.imshow('Taking Images', img)
-                #cv2.imwrite(os.path.join(path,name + "." + str(serial) + "." + Id + '.' + str(sampleNum) + ".jpg"),
-                #            gray[y:y + h, x:x + w])
-                #cv2.imshow('Taking Images', img)
-                
-                #imgname = os.path.join(IMAGES_PATH,labels,labels+'.'+'{}.jpg'.format(str(uuid.uuid1())))
-                #img_name = os.path.join(path,name + "." + str(serial) + "." + Id + '.' + str(sampleNum) + ".jpg")
-                img_name = os.path.join("TrainingImage/ " + name + "." + str(serial) + "." + Id + '.' + str(sampleNum) + ".jpg")
-                storeImage = im = open(img_name, 'rb').read()
-                
-                conn.execute("INSERT INTO images(imgname, img, userId, createdOn) VALUES(?,?,?,?)",(img_name , sqlite3.Binary(storeImage),Id,currentDateTime))
+    print("Here")
 
-                print("{} written!".format(img_name))
-                conn.commit()
-                # display the frame
-                
-            # wait for 100 miliseconds
-            if cv2.waitKey(100) & 0xFF == ord('q'):
-                break
-            # break if the sample number is morethan 100
-            elif sampleNum > 4:
-                break
-            #t_end = t_end + 1
+    #if ((name.isalpha()) or (' ' in name)):
+    cam = cv2.VideoCapture(0)
+    harcascadePath = "haarcascade_frontalface_default.xml"
+    detector = cv2.CascadeClassifier(harcascadePath)
+    sampleNum = 0
+    t_end = 1
+    while True:
+        ret, img = cam.read()
+        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        faces = detector.detectMultiScale(gray, 1.3, 5)
+        for (x, y, w, h) in faces:
+            #currentDateTime = datetime.datetime.now()
+            cv2.rectangle(img, (x, y), (x + w, y + h), (0, 255, 0), 2)
+            # incrementing sample number
+            sampleNum = sampleNum + 1
+            # saving the captured face in the dataset folder TrainingImage
+            cv2.imwrite("TrainingImage/ " + name + "." + str(serial) + "." + Id + '.' + str(sampleNum) + ".jpg",
+                        gray[y:y + h, x:x + w])
+            # display the frame
+            cv2.imshow('Taking Images', img)                
+            img_name = os.path.join("TrainingImage/ " + name + "." + str(serial) + "." + Id + '.' + str(sampleNum) + ".jpg")
+            storeImage = im = open(img_name, 'rb').read()                
+            conn.execute("INSERT INTO images(imgname, img, userId, createdOn) VALUES(?,?,?,?)",(img_name , sqlite3.Binary(storeImage),Id,currentDateTime))
+            print("{} written!".format(img_name))
+            conn.commit()
+            # display the frame                
+        # wait for 100 miliseconds
+        if cv2.waitKey(100) & 0xFF == ord('q'):
+            break
+        # break if the sample number is morethan 100
+        elif sampleNum > 4:
+            break
         cam.release()
         cv2.destroyAllWindows()
+        print("Here")
+        """
         res = "Images Taken for ID : " + Id
         row = [serial, '', Id, '', name]
+        
         with open('StudentDetails/StudentDetails.csv', 'a+') as csvFile:
             writer = csv.writer(csvFile)
             writer.writerow(row)
         csvFile.close()
-        #message1.configure(text=res)
     else:
         if (name.isalpha() == False):
-            res = "Enter Correct name"
-            #message.configure(text=res)
+            res = "Enter Correct name"""
+            
 ########################################################################################
 #$$$$$$$$$$$$$
 def TrainImages():
     name = (txt2.get())
-    #parent_dir = "TrainingImage/"
-    #path = os.path.join(parent_dir, name)
     check_haarcascadefile()
     assure_path_exists("Pass_Train/")
     recognizer = cv2.face.LBPHFaceRecognizer_create()
@@ -474,9 +459,7 @@ def TrainImages():
         mess._show(title='No Registrations', message='Please Register someone first!!!')
         return
     recognizer.save("Pass_Train/Trainner.yml")
-    res = "Profile Saved Successfully"
-    #message1.configure(text=res)
-    #message.configure(text='Total Registrations till now  : ' + str(ID[0]))
+    res = "Profile Saved Successfully"    
 
 ############################################################################################3
 #$$$$$$$$$$$$$
@@ -500,26 +483,17 @@ def getImagesAndLabels(path):
         faces_samples.append(imageNp)
         Ids.append(ID)
         #cv2.imshow("training",faceNp)
-        cv2.waitKey(10)
-        """
-        if os.path.split(image_path)[-1].split(".")[-1] !='jpg':
-            continue
-        image_id = int(os.path.split(image_path)[-1].split(".")[1])
-        faces = detector.detectMultiScale(imageNp)
-        for (x, y, w, h) in faces:
-            faces_samples.append(imageNp[y:y + h, x:x + w])
-            Ids.append(image_id)"""
+        cv2.waitKey(10)        
     return faces_samples, Ids
 ###########################################################################################
 #$$$$$$$$$$$$$
 def TrackImages():
-    check_haarcascadefile()
+    print("here")
+    check_haarcascadefile()    
     assure_path_exists("Attendance/")
     assure_path_exists("StudentDetails/")
     assure_path_exists("AttendanceImages/")
-    currentDateTime = datetime.datetime.now()
-    """for k in tb.get_children():
-        tb.delete(k)"""
+    currentDateTime = datetime.datetime.now()    
     msg = ''
     i = 0
     j = 0
@@ -531,8 +505,7 @@ def TrackImages():
         mess._show(title='Data Missing', message='Please click on Save Profile to reset data!!')
         return
     harcascadePath = "haarcascade_frontalface_default.xml"
-    faceCascade = cv2.CascadeClassifier(harcascadePath);
-
+    faceCascade = cv2.CascadeClassifier(harcascadePath)
     cam = cv2.VideoCapture(0)
     font = cv2.FONT_HERSHEY_SIMPLEX
     col_names = ['Id', '', 'Name', '', 'Date', '', 'Time']
@@ -556,75 +529,22 @@ def TrackImages():
             print(profile)
             if(profile!=None):
                 cv2.putText(im,"Name : "+str(profile[1]),(x,y+h+20),fontface, fontscale, fontcolor)
-                img_name = ("AttendanceImages/ " + str(profile[1]) + "." + str(profile[2]) + "." + str(currentDateTime) + ".jpg")
-                    
+                img_name = ("AttendanceImages/ " + str(profile[1]) + "." + str(profile[2]) + "." + str(currentDateTime) + ".jpg")                    
                 cv2.imwrite("AttendanceImages/ " + str(profile[1]) + "." + str(profile[2]) + "." + str(currentDateTime) + ".jpg",
                             gray[y:y + h, x:x + w])
-                storeImage = im = open(img_name, 'rb').read()
-                #storeImage = im = open(img_name, 'rb').read()
+                storeImage = im = open(img_name, 'rb').read()                
                 conn.execute("INSERT INTO attendance(userId, img,createdOn) VALUES(?,?,?)",(str(profile[2]) , sqlite3.Binary(storeImage) ,currentDateTime))
                 print("{} written!".format(img_name))
-                conn.commit()
-                #csvFile1.close()
+                conn.commit()                
                 cam.release()
                 cv2.destroyAllWindows()
                 maskdetection()
-                break
-                #cv2.putText(img,"Age : "+str(profile[2]),(x,y+h+45),fontface, fontscale, fontcolor);
-                #cv2.putText(img,"Gender : "+str(profile[3]),(x,y+h+70),fontface, fontscale, fontcolor); 
-                #cv2.putText(img,"Criminal Records : "+str(profile[4]),(x,y+h+95),fontface, fontscale, fontcolor);
+                break                
             else:
-                cv2.putText(im,"Name : Unknown",(x,y+h+20),fontface, fontscale, fontcolor)
-                #cv2.putText(img,"Age : Unknown",(x,y+h+45),fontface, fontscale, fontcolor);
-                #cv2.putText(img,"Gender : Unknown",(x,y+h+70),fontface, fontscale, fontcolor);
-                #cv2.putText(img,"Criminal Records : Unknown",(x,y+h+95),fontface, fontscale, fontcolor);
-            """
-            if (conf < 50):
-                ts = time.time()
-                date = datetime.datetime.fromtimestamp(ts).strftime('%d-%m-%Y')
-                timeStamp = datetime.datetime.fromtimestamp(ts).strftime('%H:%M:%S')
-                aa = df.loc[df['SERIAL NO.'] == serial]['NAME'].values
-                ID = df.loc[df['SERIAL NO.'] == serial]['ID'].values
-                ID = str(ID)
-                ID = ID[1:-1]
-                bb = str(aa)
-                bb = bb[2:-2]
-                attendance = [str(ID), '', bb, '', str(date), '', str(timeStamp)]
-                ts = time.time()
-                date = datetime.datetime.fromtimestamp(ts).strftime('%d-%m-%Y')
-                exists = os.path.isfile("Attendance/Attendance_" + date + ".csv")
-                if exists:
-                    with open("Attendance/Attendance_" + date + ".csv", 'a+') as csvFile1:
-                        writer = csv.writer(csvFile1)
-                        writer.writerow(attendance)
-                    csvFile1.close()
-                else:
-                    with open("Attendance/Attendance_" + date + ".csv", 'a+') as csvFile1:
-                        writer = csv.writer(csvFile1)
-                        writer.writerow(col_names)
-                        writer.writerow(attendance)
-                    csvFile1.close()
-                with open("Attendance/Attendance_" + date + ".csv", 'r') as csvFile1:
-                    reader1 = csv.reader(csvFile1)
-                    for lines in reader1:
-                        i = i + 1
-                        if (i > 1):
-                            if (i % 2 != 0):
-                                iidd = str(lines[0]) + '   '
-                                tb.insert('', 0, text=iidd, values=(str(lines[2]), str(lines[4]), str(lines[6])))
-                csvFile1.close()
-                cam.release()
-                cv2.destroyAllWindows()
-                maskdetection()
-                break
-            else:
-                Id = 'Unknown'
-                bb = str(Id)
-            cv2.putText(im, str(bb), (x, y + h), font, 1, (0, 251, 255), 2)"""
+                cv2.putText(im,"Name : Unknown",(x,y+h+20),fontface, fontscale, fontcolor)                
         cv2.imshow('Taking Attendance', im)
         if (cv2.waitKey(1) == ord('q')):
-            break
-                    
+            break                    
     #csvFile1.close()
     cam.release()
     cv2.destroyAllWindows()
@@ -635,7 +555,6 @@ window = Tk()
 window.title("Face and Facemask Recognition Based Attendance System")
 window.geometry("1223x729")
 window.configure(bg = "#FFFFFF")
-
 
 canvas = Canvas(
     window,
@@ -704,14 +623,14 @@ txt2.place(
 
 button_image_1 = PhotoImage(
     file=relative_to_assets("button_1.png"))
-quitWindow  = Button(
+quitWindow = Button(
     image=button_image_1,
     borderwidth=0,
     highlightthickness=0,
     command=window.destroy,
     relief="flat"
 )
-quitWindow .place(
+quitWindow.place(
     x=57.0,
     y=591.0,
     width=307.34075927734375,
@@ -724,7 +643,7 @@ trackImg = Button(
     image=button_image_2,
     borderwidth=0,
     highlightthickness=0,
-    command=TrackImages,
+    command=TrackImages,    
     relief="flat"
 )
 trackImg.place(
@@ -736,9 +655,10 @@ trackImg.place(
 
 button_image_3 = PhotoImage(
     file=relative_to_assets("button_3.png"))
-takeImg  = Button(
+takeImg = Button(
     image=button_image_3,
     borderwidth=0,
+    activebackground = "green",
     highlightthickness=0,
     command=TakeImages,
     relief="flat"
@@ -755,6 +675,7 @@ button_image_4 = PhotoImage(
 trainImg = Button(
     image=button_image_4,
     borderwidth=0,
+    activebackground = "green",
     highlightthickness=0,
     command=psw,
     relief="flat"
@@ -825,6 +746,7 @@ button_image_5 = PhotoImage(
 clearButton = Button(
     image=button_image_5,
     borderwidth=0,
+    activebackground = "green",
     highlightthickness=0,
     command=clear,
     relief="flat"
